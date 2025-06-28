89c5324ef3dd6c46d642be4efae031da7f40eca2
satwikfudotech <satwik@fudotech.in>
2025-06-28 19:36:08 +0530

    Delete currency directory

--- currency/convet.go
+++
@@ -1,9 +0,0 @@
-package currency
-
-func ConvertWithoutPointers(amount, fromRate, toRate float64) float64 {
-	return amount * (toRate / fromRate)
-}
-
-func ConvertWithPointers(amount float64, fromRate, toRate *float64) float64 {
-	return amount * (*toRate / *fromRate)
-}
